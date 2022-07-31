acfd6560283276bfdc9d4dc000d2656d97a155b8
tolatolatop <tolatolatop@gmail.com>
2022-07-31 16:23:57 +0800

    show info

---
+++ list_info.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022/7/31 16:21
+# @Author  : tolatolatop
+# @File    : list_info.py
+import json
+import pathlib
+import sys
+
+import yaml
+
+
+def main():
+    for p in pathlib.Path(sys.argv[1]).glob('*.json'):
+        with p.open() as f:
+            t = json.load(f)
+            yaml.safe_dump(t, sys.stdout)
+            print()
+
+
+if __name__ == "__main__":
+    main()
